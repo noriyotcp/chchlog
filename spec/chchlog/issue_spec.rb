95fce3895995d8e9083220c03bf7a3f3b673caea
noriyotcp <noriyo.akita@gmail.com>
2017-03-23 05:31:05 +0900

    [issue_spec] Create url for github pulls dynamically

--- spec/chchlog/issue_spec.rb
+++ spec/chchlog/issue_spec.rb
@@ -1,21 +1,30 @@
 require 'spec_helper'
 
 RSpec.describe Chchlog::Issue do
-  # pr_num = %x( \git log --merges --oneline --reverse --ancestry-path #{hash}...HEAD | grep 'Merge pull request #' | grep -oE '#[[:digit:]]+' | head -n1 | cut -b 2- )
-  # Fix: Review the following line to support git ssh url
-  # pull_url = %x( git remote get-url --push origin ).strip.gsub(/(.git)/, "/pulls/")
+  # NOTE: the following line may be used in other files
+  # pull_num = %x( \git log --merges --oneline --reverse --ancestry-path #{hash}...HEAD | grep 'Merge pull request #' | grep -oE '#[[:digit:]]+' | head -n1 | cut -b 2- )
 
   # http://stackoverflow.com/questions/1542945/testing-modules-in-rspec
   # http://stackoverflow.com/questions/1542945/testing-modules-in-rspec#comment68421749_10802518
   # http://qiita.com/tq_jappy/items/ed56b0f4a20500252461
   let(:chchlog_issue) { Class.new { extend Chchlog::Issue } }
 
-  # TODO: These are just dummies!
-  pr_num = 1
-  pull_url = "https://github.com/UteroOS/utero/pulls/#{pr_num}"
+  target_dir = %x( git rev-parse --show-toplevel).strip + "/spec/chchlog_test_repo/"
+  Dir.chdir target_dir
+
+  # e.g. git@github.com:noriyotcp/chchlog_test_repo.git
+  owner_and_repo_source = %x( git remote get-url --push origin )
+  # e.g. notiyotcp/chchlog_test_repo/pulls/
+  owner_and_repo_source.strip
+                       .slice!(/https:\/\/github.com\/|git@github.com:/)
+                       .gsub(/(.git)/, "/pulls/")
+
+  # pull_num is just dummy
+  pull_num = 1
+  pull_url = "https://github.com/#{owner_and_repo_source}#{pull_num}"
 
   it 'returns a text representing the receiver' do
-    issue = chchlog_issue.generate(pr_num, pull_url)
-    expect(issue).to eq '[#1](https://github.com/UteroOS/utero/pulls/1)'
+    issue = chchlog_issue.generate(pull_num, pull_url)
+    expect(issue).to eq "[##{pull_num}](#{pull_url})"
   end
 end
