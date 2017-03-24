bc12c1ac8231eb497f05b2c6a3b8a24046cbb43c
noriyotcp <noriyo.akita@gmail.com>
2017-03-25 01:11:39 +0900

    [change_group_spec] Use Chchlog::Issue for testing

--- spec/chchlog/change_group_spec.rb
+++ spec/chchlog/change_group_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 RSpec.describe Chchlog::ChangeGroup do
+  let(:chchlog_issue) { Class.new { extend Chchlog::Issue } }
   let(:chchlog_change) { Class.new { extend Chchlog::Change } }
   let(:chchlog_change_group) { Class.new { extend Chchlog::ChangeGroup } }
 
@@ -12,9 +13,11 @@ RSpec.describe Chchlog::ChangeGroup do
   end
 
   context "when it has an issue" do
+    let(:issue) { chchlog_issue.generate(1, 'https://github.com/noriyotcp/chchlog/pull/1') }
+
     let(:changes) do
       [
-        chchlog_change.generate_subject('subject', '[#1](https://github.com/noriyotcp/chchlog/pull/1)'),
+        chchlog_change.generate_subject('subject', issue),
         chchlog_change.generate_body('body')
       ]
     end
