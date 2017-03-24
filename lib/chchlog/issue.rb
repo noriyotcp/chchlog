573a29c72bc37d39872cd4b507c8b3b47636c948
noriyotcp <noriyo.akita@gmail.com>
2017-03-25 01:38:18 +0900

    [issue] Fix to omit to_s from the line using NTYChangeLog::Issue.new
    use to_s for the returned string on testing

--- lib/chchlog/issue.rb
+++ lib/chchlog/issue.rb
@@ -3,7 +3,7 @@ require 'nty_change_log'
 module Chchlog
   module Issue
     def generate(pr_num, pull_url)
-      NTYChangeLog::Issue.new(pr_num, pull_url).to_s
+      NTYChangeLog::Issue.new(pr_num, pull_url)
     end
   end
 end
