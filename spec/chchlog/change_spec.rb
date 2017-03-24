d5bb93795df61048c3ec3bdcffe9133e45730828
noriyotcp <noriyo.akita@gmail.com>
2017-03-25 02:11:29 +0900

    [change_spec] Deprecate an example for body, it only tests whether a issue is or not

--- spec/chchlog/change_spec.rb
+++ spec/chchlog/change_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe Chchlog::Change do
   let(:chchlog_change) { Class.new { extend Chchlog::Change } }
 
   context "when it doesn't have an issue" do
-    it 'returns a text representing the receiver as a body' do
+    it 'returns a text representing the receiver' do
       change = chchlog_change.generate('change', nil)
-      expect('  ' + change.to_s).to eq "  * change"
+      expect(change.to_s).to eq "* change"
     end
   end
 
   context "when it has an issue" do
-    it 'returns a text representing the receiver as a subject' do
+    it 'returns a text representing the receiver' do
       issue = chchlog_issue.generate(1, 'https://github.com/noriyotcp/chchlog/pull/1')
       change = chchlog_change.generate('change', issue)
       expect(change.to_s).to eq "* change [#1](https://github.com/noriyotcp/chchlog/pull/1)"
