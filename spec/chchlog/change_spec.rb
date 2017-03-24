3d0ef195ad38e4accb503234f9b8e983b945578d
noriyotcp <noriyo.akita@gmail.com>
2017-03-24 20:44:04 +0900

    Move the line for implementation from spec to chchlog.rb as comments

--- spec/chchlog/change_spec.rb
+++ spec/chchlog/change_spec.rb
@@ -11,37 +11,21 @@ RSpec.describe Chchlog::Change do
     Dir.chdir root_dir
   end
 
-  # NOTE: the following line may be used in other files
-  # pull_num = %x( \git log --merges --oneline --reverse --ancestry-path #{hash}...HEAD | grep 'Merge pull request #' | grep -oE '#[[:digit:]]+' | head -n1 | cut -b 2- )
-
   let(:chchlog_issue) { Class.new { extend Chchlog::Issue } }
   let(:chchlog_change) { Class.new { extend Chchlog::Change } }
 
-  # e.g. git@github.com:noriyotcp/chchlog_test_repo.git
-  owner_and_repo = %x( git remote get-url --push origin )
-  # e.g. notiyotcp/chchlog_test_repo/pulls/
-  owner_and_repo.strip
-                       .slice!(/https:\/\/github.com\/|git@github.com:/)
-                       .gsub(/(.git)/, "/pulls/")
-
-  # commit_message is just dummy
-  commit_message = "change"
-
   context "when it doesn't have an issue" do
     it 'returns a text representing the receiver' do
-      change = chchlog_change.generate(commit_message, nil)
+      change = chchlog_change.generate('change', nil)
       expect(change).to eq "* change"
     end
   end
 
   context "when it has an issue" do
-    pull_num = 1
-    pull_url = "https://github.com/#{owner_and_repo}#{pull_num}"
-
     it 'returns a text representing the receiver' do
-      issue = chchlog_issue.generate(pull_num, pull_url)
-      change = chchlog_change.generate(commit_message, issue)
-      expect(change).to eq "* change [##{pull_num}](#{pull_url})"
+      issue = chchlog_issue.generate(1, 'https://github.com/noriyotcp/chchlog/pull/1')
+      change = chchlog_change.generate('change', issue)
+      expect(change).to eq "* change [#1](https://github.com/noriyotcp/chchlog/pull/1)"
     end
   end
 end
--- spec/chchlog/issue_spec.rb
+++ spec/chchlog/issue_spec.rb
@@ -11,28 +11,13 @@ RSpec.describe Chchlog::Issue do
     Dir.chdir root_dir
   end
 
-  # NOTE: the following line may be used in other files
-  # pull_num = %x( \git log --merges --oneline --reverse --ancestry-path #{hash}...HEAD | grep 'Merge pull request #' | grep -oE '#[[:digit:]]+' | head -n1 | cut -b 2- )
-
   # http://stackoverflow.com/questions/1542945/testing-modules-in-rspec
   # http://stackoverflow.com/questions/1542945/testing-modules-in-rspec#comment68421749_10802518
   # http://qiita.com/tq_jappy/items/ed56b0f4a20500252461
   let(:chchlog_issue) { Class.new { extend Chchlog::Issue } }
 
-  # e.g. git@github.com:noriyotcp/chchlog_test_repo.git
-  owner_and_repo = %x( git remote get-url --push origin )
-  # e.g. notiyotcp/chchlog_test_repo/pulls/
-  owner_and_repo.strip
-                       .slice!(/https:\/\/github.com\/|git@github.com:/)
-                       .gsub(/(.git)/, "/pulls/")
-
-  # pull_num is just dummy
-  pull_num = 1
-  pull_url = "https://github.com/#{owner_and_repo}#{pull_num}"
-
   it 'returns a text representing the receiver' do
-    issue = chchlog_issue.generate(pull_num, pull_url)
-    expect(issue).to eq "[##{pull_num}](#{pull_url})"
+    issue = chchlog_issue.generate(1, 'https://github.com/noriyotcp/chchlog/1')
+    expect(issue).to eq "[#1](https://github.com/noriyotcp/chchlog/1)"
   end
-
 end
