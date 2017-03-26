d8fcb6d25ee820ff1e840cbe6de8bfa87e707a53
noriyotcp <noriyo.akita@gmail.com>
2017-03-26 21:58:59 +0900

    [change_group] Returns a simple string instead of NTYChangeLog::ChangeGroup object

--- lib/chchlog/change_group.rb
+++ lib/chchlog/change_group.rb
@@ -3,7 +3,10 @@ require 'nty_change_log'
 module Chchlog
   module ChangeGroup
     def generate(name, changes)
-      NTYChangeLog::ChangeGroup.new(name, changes)
+      return <<~EOS.chomp
+        ### #{name}
+        #{changes.join("\n")}
+      EOS
     end
   end
 end
